60223adfb8a0d6906584ea4fe1a882974933b004
rxnchel06 <njlc0403@leeds.ac.uk>
2026-03-16 11:57:33 +0000

    worked on joins

--- session_2/1_aggregators/tasks.sql
+++ session_2/1_aggregators/tasks.sql
@@ -7,3 +7,5 @@
 -- 2. Load this script: .read tasks.sql
 -- 3. Exit SQLite: .exit
 
+--1. Find the average population for each continent.
+SELECT AVG(Population) as average_population, Continent from countries GROUP BY continent;
--- session_2/2_joins/tasks.sql
+++ session_2/2_joins/tasks.sql
@@ -7,3 +7,22 @@
 -- 2. Load this script: .read tasks.sql
 -- 3. Exit SQLite: .exit
 
+--1. List all posts that have been deleted.
+--SELECT * FROM posts where deleted = 1;
+
+--2. List all users whose usernames begin with 'a'.
+--SELECT * FROM users WHERE username LIKE 'a%';
+
+--3. Find all posts and the usernames of those who posted them whose content contains the word "small".
+SELECT users.username, posts.* 
+FROM 
+users JOIN posts
+ON users.id = posts.user_id
+where content like '%small%';
+--4. Return each non-deleted post and the username of the person who wrote in, in date order.
+--5. For each user, return their username and the total number of posts they have made.
+--6. Modify the previous query to return only users who have authored more than 3 posts.
+--7. For each user, return their username and the number of users they follow, including users who follow no one.
+--8. List the top 5 most-followed users.
+--9. Return a list showing the username of the follower and the username of the followed user for each follow relationship.
+--10. For a given user (christopherprice), return all non-deleted posts written by users they follow, ordered from most recent to oldest.
